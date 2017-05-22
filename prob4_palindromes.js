c06a272a378acc20ff69524185251c0783858455
londes <william@chartbeat.com>
2017-05-22 17:52:11 -0400

    problem 4

---
+++ prob4_palindromes.js
@@ -0,0 +1,34 @@
+let isPalindrome = function (testNumber) {
+  let testString = testNumber.toString();
+  let reverseTest = testString.split("").reverse().join("");
+  return (testString === reverseTest);
+}
+
+let getThreeDigitProducts = function () {
+  let products = [];
+  for (let i=999; i>600; i--) {
+    for (let j=999; j>600; j--) {
+      if (isPalindrome(i*j)){
+        //console.log(i*j);
+        products.push(i*j);
+      }
+    }
+  }
+  sortThreeDigitProducts(products);
+  return products;
+}
+
+let sortThreeDigitProducts = function (productsArray) {
+  return productsArray.sort(sortNumbers);
+}
+
+let sortNumbers = function (a, b) {
+  return a - b;
+}
+
+console.log(isPalindrome(9009));
+console.log(isPalindrome(9049));
+console.log(isPalindrome(9010109));
+console.log(sortThreeDigitProducts([12,16,99,4,36]).pop());
+
+console.log(getThreeDigitProducts().pop());
