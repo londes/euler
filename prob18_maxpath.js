e06bf032ee36970337fc3c01b66d0345085a2eaf
londes <william@chartbeat.com>
2017-10-16 18:18:30 -0400

    updated prob18

--- prob18_maxpath.js
+++ prob18_maxpath.js
@@ -1,11 +1,14 @@
+// we know the height of the tree already
 let tree = [];
 let level = [];
 let height = 4;
 let width = 1;
 
+// we assume this input is a tree
 let treeNums = [3,7,4,2,4,6,8,5,9,3];
 
-function convertStringToTree(numArray) {
+//converts array of nums to array of arrays in a tree format
+function convertNumsToTree(numArray) {
   let loc = 0;
   for (var i = 0; i < height; i++) {
       for (var j = 0; j < width; j++) {
@@ -19,7 +22,9 @@ function convertStringToTree(numArray) {
   return tree;
 }
 
+// converts tree array to array of objects with tree relationships (is this necessary?)
+function buildRelations(treeArray) {
+  return;
+}
 
-console.log(convertStringToTree(treeNums));
-
-tree.push({});
+console.log(convertNumsToTree(treeNums));
