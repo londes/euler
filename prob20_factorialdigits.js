eaa2c3b46069c9422062cf94ba84b616a93fb67f
londes <wedozeez@gmail.com>
2022-02-17 13:47:09 -0500

    this program works and solves the problem but doesn't handle scientific notation for huge numbers

--- prob20_factorialdigits.js
+++ prob20_factorialdigits.js
@@ -1,3 +1,29 @@
-let test = "this is a test";
+let number = 10;
+let factorialNums = [];
+let initialValue = 1;
 
-console.log(test);
+// generate an array of numbers based on the starting value
+for (i=number;i>0;i--){
+    factorialNums.push(i);
+}
+
+console.log(factorialNums);
+
+// reduce the array of numbers to the product of all numbers in the array
+let factorialProduct = factorialNums.reduce(
+    (previousValue, currentValue) => previousValue * currentValue
+);
+
+console.log("product: " + factorialProduct);
+
+// take the product of all numbers in array, and convert to array of the digits of that product
+let factorialDigits = Array.from(factorialProduct.toString()).map(Number);
+
+console.log("factorial digits: " + factorialDigits);
+
+// sum all of the digits in the product
+let factorialDigitSum = factorialDigits.reduce(
+    (previousValue, currentValue) => previousValue + currentValue
+);
+
+console.log("sum: " + factorialDigitSum);
