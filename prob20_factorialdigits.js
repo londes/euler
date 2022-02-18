9aea6f403095c10cc1a8ae16e01a678274ff6ba0
londes <wedozeez@gmail.com>
2022-02-18 17:38:16 -0500

    got my js utils file imported but it turns out it's buggy and doesn't fully work. convinced this code is good for prob20 but euler says otherwise

--- prob20_factorialdigits.js
+++ prob20_factorialdigits.js
@@ -1,4 +1,7 @@
-let snUtil = require('../utils/non_scientific_notation');
+// import our utility
+// leaving this commented out for now because we can just parse the integers from the array
+
+// let snUtil = require('../utils/non_scientific_notation');
 
 let number = 100;
 let factorialNums = [];
@@ -8,20 +11,29 @@ for (i=number;i>0;i--){
     factorialNums.push(i);
 }
 
-console.log(factorialNums);
+console.log("outputs for number equals: " + number);
 
 // reduce the array of numbers to the product of all numbers in the array
 let factorialProduct = factorialNums.reduce(
     (previousValue, currentValue) => previousValue * currentValue
 );
-
-factorialProduct = snUtil.toFixed(factorialProduct);
 console.log("product: " + factorialProduct);
 
+// reference to our busted util that we imported with require()
+//factorialProduct = snUtil.toFixed(factorialProduct);
+
 // take the product of all numbers in array, and convert to array of the digits of that product
-let factorialDigits = Array.from(factorialProduct.toString()).map(Number);
+// the check for 'e' and the filter is to account for numbers returned in scientific notation
+let factorialDigits = Array.from(((factorialProduct.toString()).split('e'))[0]).map(Number);
+
+factorialDigits = factorialDigits.filter(function (digit) {
+    return !Number.isNaN(digit);
+});
 
 console.log("factorial digits: " + factorialDigits);
+
+// please note that this is a janky, ad-hoc solution to get the solution for 100 specifically, as our
+// non_scientific_notation util is changing the last number in the sequence sometimes, for some reason.
 
 // sum all of the digits in the product
 let factorialDigitSum = factorialDigits.reduce(
