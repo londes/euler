0b4ca7aee7d8843979aa6fce4d6fea1dd4ad19ec
William Purcell <william@chartbeat.com>
2022-03-13 16:20:36 -0400

    we're in business, it's almost always some dumb syntax error

--- prob21_amicablenumbers.js
+++ prob21_amicablenumbers.js
@@ -2,15 +2,17 @@
 let n = 100;
 console.log('our starting number is: ' + n);
 
-// determine all of the factors for each number that are less than that number
-let digitsArray = [];
+// determine all of the numbers that are less than the starting number
+// and store them
 
+let digitsArray = [];
 for (let i = 1; i<n; i++) {
     digitsArray.push(i);
 }
-
 console.log('the digits less than ' + n + ' are ' + digitsArray);
 
+// function that accepts a number, determines all of its factors,
+// then sums all of the factors and returns that sum
 function getFactorsSum (n) {
     console.log('we are in getFactorsSum for ' + n);
     let factorsArray = [];
@@ -24,16 +26,16 @@ function getFactorsSum (n) {
         return 1;
     }
     console.log('the sum of the factors for ' + n + ' are ' + factorsArray.reduce((a, b) => a + b));
-    return factorsArray.reduce(a, b => a + b);
+    return factorsArray.reduce((a, b) => a + b);
     
 }
 
+// store them in an array of objects
 let factorSumsDict = []
 digitsArray.forEach(value => {
+    console.log('we are in the forEach and value is: ' + value);
     factorSumsDict.push({value: getFactorsSum(value)})
 })
-
-// store them in a dictionary
 
 // for each key, check for a matching value
 
